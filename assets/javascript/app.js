74ed85968070e616cec5c53f58d9ec4535823c1a
Andrew Norris <andrewnorris@calvisitor-10-105-171-29.calvisitor.1918.berkeley.edu>
2018-08-07 09:47:21 -0700

    Initial commit

---
+++ assets/javascript/app.js
@@ -0,0 +1,13 @@
+// Create a Trivia Game
+
+// A start button will initialize the game 
+
+// When the game begins, a timer is displayed and begins couting down at the top of the page.
+
+// 8 Multiple Choice questions are displayed with 4 mutiple choice answers, using radio buttons
+
+// When a right or wrong answer is selected show a  gif
+
+// Once all questions are answered or time runs out show a scorecard showing correct, incorrect, and unanswered 
+
+// Show a play again button to re start the game
