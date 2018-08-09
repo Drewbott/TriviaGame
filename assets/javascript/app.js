62f1483d7764c1458f85679bb77c6b67370dd940
Andrew Norris <andrewnorris@Andrews-MacBook-Air.local>
2018-08-08 19:51:40 -0700

    Version 2

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -18,12 +18,18 @@
 
 // Variables go here
 // timer
+var intervalId;
+var clockRunning = false;
+var timer = 90;
 // question object?
 // counter
+
+var correct = 0;
+var unanswered=0;
 var questions = [
 {question: "The Athletics were in Philadelphia when their franchise began in the American League in 1901. Who was their manager in that season?", 
-answers: ["Connie Mack", "Jimmy Collins", "John McGraw", "Hugh Duffy"],
-correctAnswer: "Connie Mack"},
+answers: ["Connie-Mack", "Jimmy-Collins", "John-McGraw", "Hugh-Duffy"],
+correctAnswer: "Connie-Mack"},
 {question: "After a stint in Kansas City the team officially moved to Oakland in what year?",
 answers: ["1961", "1966", "1968", "1969"],
 correctAnswer: "1968"}
@@ -32,22 +38,40 @@ correctAnswer: "1968"}
 
 ];
 
-
+if (!clockRunning) {
+      
+    intervalId = setInterval(timer.count, 1000);
+    clockRunning = true;
+  }
 // Functions go here:
 // answerTracker
 var initializeGame = function(){
     for (var i = 0; i<questions.length; i++){
         var questionDiv = $("<div>");
-        questionDiv.html("<h2>"+ questions[i].question + "</h2>")
+        questionDiv.html("<h2>" + questions[i].question + "</h2>")
         for (var j = 0; j<questions[i].answers.length; j++){
-            questionDiv.append("<p>" + questions[i].answers[j] + "</p>" )
+            questionDiv.append("<input type='radio' ans="+questions[i].answers[j]+" name="+"q"+i+">" + questions[i].answers[j])
             }
         $("#questions").append(questionDiv)
-        // 
+        
     }
 }
+
+
+$("#end").on("click", function(){
+   
+    for (let i = 0; i < questions.length; i++) {   
+    if($("input[name=q"+i+"]:checked").length>0){
+        if($("input[name=q"+i+"]:checked")[0].getAttribute("ans")==questions[i].correctAnswer) {
+            correct ++;
+        }  
+    }else{
+        unanswered ++;
+    }
+    }
+})
 initializeGame()
-
-
 // showResults = function(){}
 
+
+ 
